5f74f778acc1d3ef62f0b90f27f99bcccdfb2fc6
Agoston Torok <torokagoston@gmail.com>
2017-11-08 15:12:41 +0200

    Remove unnecessary features and set up basic test (#1)
    * Add feature.txt to follow development
    
    * Updated features.txt to show progress
    
    I started implementing the testing by first removing
    everything that was not crucial
    
    * Removed unnecessary tests
    
    * Removed prompts from hook.sh
    
    Linus accepts no objectsions for now ;)
    
    * Removed reference to git.config for patterns
    
    * Remove file checking from hook
    
    * Modify hook target to commit-msg hook
    
    * Add failing base test for commit message
    
    * Updated features to show implementation is done

--- hook.sh
+++ hook.sh
@@ -9,80 +9,22 @@ fi
 
 IFS=$'\n'
 
-# http://djm.me/ask
-ask() {
-    while true; do
+check_message() {
+    local message=$1
+    local message_string=$( cat "$message" )
+    local pattern1='test'
+    local error_msg="Aborting commit. Your commit message is missing $pattern1, it is $message_string"
 
-        if [ "${2:-}" = "Y" ]; then
-            prompt="Y/n"
-            default=Y
-        elif [ "${2:-}" = "N" ]; then
-            prompt="y/N"
-            default=N
-        else
-            prompt="y/n"
-            default=
-        fi
-
-        # Ask the question (not using "read -p" as it uses stderr not stdout)
-        echo -n "$1 [$prompt] "
-
-        # Read the answer
-        read REPLY < "$TTY"
-
-        # Default?
-        if [ -z "$REPLY" ]; then
-            REPLY=$default
-        fi
-
-        # Check if the reply is valid
-        case "$REPLY" in
-            Y*|y*) return 0 ;;
-            N*|n*) return 1 ;;
-        esac
-
-    done
-}
-
-check_file() {
-    local file=$1
-    local match_pattern=$2
-
-    local file_changes_with_context=$(git diff -U999999999 -p --cached --color=always -- $file)
-
-    # From the diff, get the green lines starting with '+' and including '$match_pattern'
-    local matched_additions=$(echo "$file_changes_with_context" | grep -C4 $'^\e\\[32m\+.*'"$match_pattern")
-
-    if [ -n "$matched_additions" ]; then
-        echo -e "\n$file additions match '$match_pattern':\n"
-
-        for matched_line in $matched_additions
-        do
-            echo "$matched_line"
-        done
-
-        if ask "Include this in your commit?"; then
-            echo 'Including'
-        else
-            echo "Not committing, because $file matches $match_pattern"
-            exit 1
-        fi
-    fi
+    if ! grep -iqE "$pattern1" "$message"; then
+    echo "$error_msg" >&2
+    exit 1
+fi
 }
 
 # Actual hook logic:
 
-MATCH=$(git config --get-all hooks.confirm.match)
-if [ -z "$MATCH" ]; then
-    echo "Git-Confirm: hooks.confirm.match not set, defaulting to 'TODO'"
-    echo 'Add matches with `git config --add hooks.confirm.match "string-to-match"`'
-    MATCH='TODO'
-fi
+# regex to validate in commit msg
 
-for file in `git diff --cached -p --name-status | cut -c3-`; do
-    for match_pattern in $MATCH
-    do
-        check_file $file $match_pattern
-    done
-done
+check_message $1
+
 exit
