5783324dfe74cb1c5d27064a3eb5a34d3303782c
Agoston Torok <torokagoston@gmail.com>
2019-05-12 23:42:46 +0200

    Cut unnecessary echos

--- hook.sh
+++ hook.sh
@@ -24,7 +24,8 @@ update_xp(){
 }
 
 check_message() {
-    echo "Linus is checking your commit message ..."
+    echo ""
+    echo  -e "\033[0;31mLinus is checking your commit message ..."
 
     local message=$1 # refer as message to the first arg
     # local message_string=$( cat "$message" ) # the actual string of the message
@@ -35,6 +36,7 @@ check_message() {
         "^(Add|Cut|Fix|Bump|Make|Start|Stop|Refactor|Reformat|Optimize|Document):Why do you want to invent a starting word for your commit? Use one of Add, Fix etc. that we usually use.:254"
         "^(Add|Cut|Fix|Bump|Make|Start|Stop|Refactor|Reformat|Optimize|Document)(?!ed):Don't use past tense, your commit has not been accepted yet.:253"
         "^.{1,50}(?!.):Seriously? You expected me to read this overly verbose commit message?:252")
+    
     for KeyValPair in "${ChecksAndReasons[@]}"
         do
             # let's split the list items to parts
@@ -45,12 +47,13 @@ check_message() {
             ### DEBUG part
             # echo "$pattern $message_string"
             # echo `head -1 "$message" | grep -P "$pattern" "$1"`
-            if ! head -1 "$message" | grep -P "$pattern" "$1"; then
+            is_violating=$(! head -1 "$message" | grep -P "$pattern" "$1")
+            if [ -z "$is_violating" ]; then
                 # some check detected an error
                 action=$(get_review_action)
-                echo "$action $reason Error code: $error_code" >&2
+                echo -e "\033[0;31m$action $reason Error code: $error_code" >&2
                 update_xp -5
-                echo "For this, you earned -5XP that means you have a total of ${xp}XP now."
+                echo -e "\033[0;31mFor this, you earned -5XP that means you have a total of ${xp}XP now."
                 exit $error_code
             fi
           
@@ -58,7 +61,7 @@ check_message() {
 
     ## all good
     update_xp 1
-    echo "Good job, I give you 1XP that means you have a total of ${xp}XP now."
+    echo -e "\033[0;31mGood job, I give you 1XP that means you have a total of ${xp}XP now."
     
 }
 
